ab524875d86901b4e9afc57acb67bb89324f420c
DremeYezer <150881979+DremeYezer@users.noreply.github.com>
2023-11-15 12:29:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//   let btn = document.getElementById('klay').addEventListener('click', ()=>{
+//    btn.setAttribute('', '');
+// });
